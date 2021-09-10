351f196382caaea2e9aa0a996e6648648c2093a3
mega5800 <43865655+mega5800@users.noreply.github.com>
2021-09-10 10:27:37 +0300

    added new classes

---
+++ app/models/Cost.scala
@@ -0,0 +1,16 @@
+package models
+
+class Cost(stringID:String)
+{
+
+}
+
+/*
+* struct cost:
+* string name
+* Date date
+* Category category = enum
+* float sum
+* int ID
+* User
+* */
---
+++ app/models/CostsManagerModel.scala
@@ -0,0 +1,11 @@
+package models
+
+class CostsManagerModel
+{
+  //def getCosts(userName:String):List[Nothing] = ???
+
+  //def addNewCostToUser(userName:String, newCostToAdd:Float):Boolean = ???
+
+  //def removeCostFromCostsList() = ???
+}
+
